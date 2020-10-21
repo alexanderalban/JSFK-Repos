7db225f059c0cdbd4ea2eef127e33f3d640b8854
alexanderalban <albanalexander@gmail.com>
2020-10-21 15:36:36 -0500

    intial commit for folder, sentence worksgit status

---
+++ String Capitalization/myscripts.js
@@ -0,0 +1,13 @@
+let sentence = "hELlo THERE, hOW ARE yOu doINg?";
+
+console.log(sentence);
+
+let capitalH = sentence[0].toUpperCase();
+
+let restOfSentence = sentence.slice(1).toLowerCase();
+
+let correctedSentence = capitalH + restOfSentence;
+
+console.log(correctedSentence);
+
+// type into console!
