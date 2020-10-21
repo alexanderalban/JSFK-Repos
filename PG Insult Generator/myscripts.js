5e6f0889398bfe64b49c8ba6e7ac284a6a47cdfc
alexanderalban <albanalexander@gmail.com>
2020-10-21 17:40:56 -0500

    all generators work!

--- PG Insult Generator/myscripts.js
+++ PG Insult Generator/myscripts.js
@@ -1,3 +1,5 @@
+//Random PG Insult Generator!
+
 let randomBodyParts = ["nose", "arm", "head", "foot", "leg"];
 
 let randomAdjectives = ["hairy", "smelly", "goofy", "fluffy", "silly"];
@@ -21,3 +23,18 @@ let randomInsult = "Your " + randomBodyPart + " is more " + randomAdjective + " 
 console.log(randomInsult);
 
 //Copy into console!
+
+//Another way to declare the same log: 
+
+let randomInsultTwo = ["Your", randomBodyPart, "is", "more", 
+randomAdjective, "than", "a", randomAnimal + "'s", randomAnimalBodyPart + "."].join(" ");
+
+console.log(randomInsultTwo);
+
+//number array!
+
+let numberArray = [3, 2, 1];
+let numberSentence = numberArray[0] + " is bigger than " + numberArray[1] 
++ " is bigger than " + numberArray[2] + ".";
+
+console.log(numberSentence);
