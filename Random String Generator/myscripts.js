236715a9fb43e8d334b8e63a333b3c1b4f160cd2
alexanderalban <albanalexander@gmail.com>
2020-10-22 12:10:49 -0500

    the generator works! and I know why now!

--- Random String Generator/myscripts.js
+++ Random String Generator/myscripts.js
@@ -1,5 +1,25 @@
 const alphabetLetter = "abcdefghijklmnopqrstuvwxyz"
 
-let randomString = alphabetLetter[Math.floor(Math.random() * alphabetLetter.length)];
+let randomLetter = alphabetLetter[Math.floor(Math.random() * alphabetLetter.length)];
+
+console.log(randomLetter);
+
+// for (let i = 0; i = alphabetLetter.length; i++) {
+//     console.log(alphabetLetter[Math.floor(Math.random() * alphabetLetter.length[i])])
+// }
+
+// let i = 0;
+// while (i < 6) {
+//   console.log(alphabetLetter[Math.floor(Math.random() * i)]);
+//   i++;
+// }
+
+
+let i = 0;
+let randomString = ""
+while (i < 6) {
+  randomString += alphabetLetter[Math.floor(Math.random() * alphabetLetter.length)];
+  i++;
+}
 
 console.log(randomString);
