d5b1d7adce1206cfca13bb45f3f1e5f0c9ce1b6f
alexanderalban <albanalexander@gmail.com>
2020-10-21 19:43:06 -0500

    initial for folder, both loops work

---
+++ Power of Three Loop/myscripts.js
@@ -0,0 +1,11 @@
+for (let x = 3; x <= 10000; x = x * 3) {
+    console.log(x);
+  }
+  
+  //success! now do it with a while loop
+  
+  let y = 3;
+  while (y <= 10000) {
+    console.log(y);
+    y = y * 3;
+  }
