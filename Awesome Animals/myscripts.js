57f4099cb1ca9921a19bf7d938b29a36d37fbf6b
alexanderalban <albanalexander@gmail.com>
2020-10-21 19:56:21 -0500

    initial for folder, animals are now awesome

---
+++ Awesome Animals/myscripts.js
@@ -0,0 +1,7 @@
+let normalAnimals = ["Blue Whale", "Wolf", "Crow", "Octopus", "Grey Parrot", "Elephant", "Dolphin", "Golden"]
+
+for (let i = 0; i < normalAnimals.length; i++) {
+  normalAnimals[i] = "Awesome " + normalAnimals[i];
+}
+
+console.log(normalAnimals);
