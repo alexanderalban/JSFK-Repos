6739f6771f0664fb33f6474d72858c6c8e0b1cab
alexanderalban <albanalexander@gmail.com>
2020-10-21 19:08:18 -0500

    intial for folder, if else works

---
+++ Hello Me/myscripts.js
@@ -0,0 +1,11 @@
+const name = "Obi-Wan";
+
+if (name === "Alex") {
+  console.log("Hello me!"); 
+} else if (name === "Dad") {
+  console.log("Hi Dad!");
+} else if (name === "Obi-Wan") {
+  console.log("General Kenobi!");
+}  else {
+    console.log("Hello stranger!");
+}
