c6a88326e49a6828d1b559676a7112df63c9b4d1
alexanderalban <albanalexander@gmail.com>
2020-10-23 18:31:02 -0500

    initial, functions work!

---
+++ Arithmetic Functions/myscripts.js
@@ -0,0 +1,13 @@
+let add = (num1, num2) => {
+  return num1 + num2;
+}
+
+// console.log(add(7, 6));
+
+let multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+// console.log(multiply(7, 6));
+
+console.log(add(multiply(36325, 9824), 777));
