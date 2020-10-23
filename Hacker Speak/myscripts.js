ea4d1d7781dbc2f0998f99fd769e96cd279ebd71
alexanderalban <albanalexander@gmail.com>
2020-10-23 12:17:04 -0500

    generator works! intial commit for folder

--- Hacker Speak/myscripts.js
+++ Hacker Speak/myscripts.js
@@ -2,5 +2,17 @@ let inputSpeech = "javascript is awesome!"
 let outputSpeech = ""
 
 for (i = 0; i < inputSpeech.length; i++) {
-  
+  if (inputSpeech[i] === 'a') {
+    outputSpeech += "4";
+  } else if (inputSpeech[i] === 'e') {
+    outputSpeech += "3";
+  } else if (inputSpeech[i] === 'i') {
+    outputSpeech += "1";
+  } else if (inputSpeech[i] === 'o') {
+    outputSpeech += "0"
+  } else {
+    outputSpeech += inputSpeech[i];
+  }
 }
+
+console.log(outputSpeech);
