2aa2529dbc6bd18df59d31d104807779bb6ea0af
alexanderalban <albanalexander@gmail.com>
2020-10-23 22:35:59 -0500

    intial, animation works

---
+++ jQuery Practice/myscripts.js
@@ -0,0 +1,18 @@
+// $('h1').slideUp(2000).slideDown(2000);
+
+
+
+// for (i = 0; i < 6; i++) {
+//     $('h1').fadeOut(1000);
+//     $('h1').fadeIn(2000);
+// };
+
+
+
+let j = 0;
+while (j < 1000) {
+    $('h1').hide(1000);
+    $('h1').show(1000);
+    console.log("hello there");
+    j++;
+};
