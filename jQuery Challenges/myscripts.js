690d99582039a07c80142686f7b65c6087b7886c
alexanderalban <albanalexander@gmail.com>
2020-10-24 12:54:27 -0500

    initial, script works!

--- jQuery Challenges/myscripts.js
+++ jQuery Challenges/myscripts.js
@@ -1,2 +1,10 @@
 let powerRangersArray = ["Zack", "Kimberly", "Billy", "Trini", "Jason", "Tommy"];
 
+for(i = 0; i < powerRangersArray.length; i++) {
+    $('body').append("<p>" + powerRangersArray[i] + "</p>");
+    $('p').hide().delay(1000 * i).fadeIn(1000);
+}
+
+$('h1').text("Go Go Power Rangers!");
+
+$('p').append(" is Morphenominal!").delay(10000).fadeIn(1000);
